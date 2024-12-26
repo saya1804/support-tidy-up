4e52cbe59a71f851f68cd038b3886a04b45c30dc
“saya1804” <se94xo@gmail.com>
2024-12-26 15:36:53 +0900

    ユーザーの設定

--- support_tidy_up/app/models.py
+++ support_tidy_up/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    date_joined = None
+    groups = None
+    user_permissions = None
+
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        db_table = "users"
+        

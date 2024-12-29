55cab7a3536550596e7397f9282cbd44504fd36c
“saya1804” <se94xo@gmail.com>
2024-12-29 18:12:43 +0900

    ログインの設定

--- support_tidy_up/app/forms.py
+++ support_tidy_up/app/forms.py
@@ -13,3 +13,18 @@ class SignupForm(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("このメールアドレスはすでに登録されています")
         return email
+    
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField()
+
+
+    def clean(self):
+        print("loginformのクリーンが呼び出された")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        print(email, password)
+        self.user = authenticate(email=email, password=password)
+        if self.user is None:
+            raise forms.ValidationError("認証に失敗しました")
+        return self.cleaned_data
--- support_tidy_up/app/views.py
+++ support_tidy_up/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from app.forms import SignupForm
+from app.forms import SignupForm, LoginForm
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
@@ -32,8 +32,18 @@ class SignupView(View):
 class LoginView(View):
     def get(self, request):
         return render(request, "login.html")
+    def post(self, request):
+        print(request.POST)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            login(request, form.user)
+            return redirect("home")
+        return render(request, "login.html", context={
+            "form":form
+        })
 
 
-class HomeView(View):
+class HomeView(LoginRequiredMixin, View):
+    login_url = "login"
     def get(self, request):
         return render(request, "home.html")
